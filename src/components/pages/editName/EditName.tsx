ed3670c6ae05001d338ad6192dd11e57b9944cbb
Java Babykish <bkrevsun@gmail.com>
2023-04-28 05:24:23 +0400

    dashboard

--- src/components/pages/editName/EditName.tsx
+++ src/components/pages/editName/EditName.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import GoBack from '../../common/goBack/GoBack';
-import { PROVIDE_EMAIL_ROUTE } from '../../../utils/consts';
+import { PROVIDE_EMAIL_ROUTE, PROFILE_ROUTE } from '../../../utils/consts';
 import { Container, Title, Input, StyledButton } from './components'
 import accountService from '../../../service/accountService';
 import { useNavigate } from 'react-router-dom';
