7dae468d05a771493a54e95671e19083e03bdf7e
Java Babykish <bkrevsun@gmail.com>
2023-05-21 21:47:26 +0400

    small fixes

--- src/components/modals/cropSelfie/CropSelfie.tsx
+++ src/components/modals/cropSelfie/CropSelfie.tsx
@@ -140,7 +140,7 @@ const CropSelfie = (props: { selfie: File |null , page:string}) => {
         showGrid={false}
         cropShape={'round'}
         objectFit="vertical-cover"
-        initialCroppedAreaPercentages={{ width: 248, height: 248, x: 0, y: 0   }}
+        initialCroppedAreaPercentages={{ width: 200, height: 200, x: 15, y: 25   }}
         classes={
           {
           containerClassName: 'containerClassName',
